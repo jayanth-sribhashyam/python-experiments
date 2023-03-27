93ae74eca5a759df3e2d6bdd2c2171f863eca967
jayanth-sribhashyam <129029085+jayanth-sribhashyam@users.noreply.github.com>
2023-03-27 10:13:05 +0530

    Update exp1.py

--- exp1.py
+++ exp1.py
@@ -1,5 +1,8 @@
+#example python program to recieve multiple inputs simultaneously
+#no .of test cases
 t=int(input())
 for i in range(0,t):
+  #using map function we enter multiple inputs
   x,y=map(int,input().split())
   print("sum =",x+y)
   print("power =",x**y)
